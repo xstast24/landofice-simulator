26d5d8d03945e41bc53d2d049df7ea262fcc61dc
Jakub <jochlik@email.cz>
2022-05-23 22:24:37 +0200

    frakce 0

--- simulator_engine.php
+++ simulator_engine.php
@@ -157,7 +157,7 @@ class Jednotka {
 
 				$this->pocetUtoku = $jednotka->pocetUtoku * 1;
 				
-				#frakce: 1 - Dralgar, 2 - vulkan, 3 - Aether, 4 - Dreadd, 5 - Dhar, 6 - Ghoro, 7 - crinis, 8 - ascendacy
+				#frakce: 0 - nezařazeno, 1 - Dralgar, 2 - vulkan, 3 - Aether, 4 - Dreadd, 5 - Dhar, 6 - Ghoro, 7 - crinis, 8 - ascendacy
 				$this->frakce = $jednotka->frakce * 1;
 
 				foreach ($jednotka->schopnosti->schopnost as $schopnost){
