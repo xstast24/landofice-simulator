f24d8ab8f03873f178e5c2eb81a5c32442f86df4
Jakub <jochlik@email.cz>
2022-05-24 18:40:15 +0200

    parsovani statů

--- utils/unit_parser.py
+++ utils/unit_parser.py
@@ -33,14 +33,48 @@ def extract_data_from_html_table(raw_data: str) -> list:
 
         if row_length == 2:
             fraction = set_fraction(row)
-        else:
+        elif row_length == 16:
             unit_list.append(parse_html_row(row, fraction))
 
     return unit_list
 
 
 def parse_html_row(row: str, fraction: str) -> dict:
-    pass
+    print("\n")
+    for r in row:
+        print(r)
+    unit = {
+        "id": row.select_one(":nth-child(2)").getText(),
+        "nazev": row.select_one(":nth-child(3)").getText(),
+        "utok": row.select_one(":nth-child(4)").getText(),
+        "obrana": row.select_one(":nth-child(5)").getText(),
+        "damage": row.select_one(":nth-child(6)").getText(),
+        "zivoty": row.select_one(":nth-child(7)").getText(),
+        "iniciativa": row.select_one(":nth-child(8)").getText(),
+        "typUtoku": row.select_one(":nth-child(9)").getText(),
+        "hodnota": row.select_one(":nth-child(10)").getText(),
+        "stav": "1",
+        "frakce": fraction,
+        "pocetUtoku": "1",
+        "schopnosti": {}
+    }
+    magic = row.select_one(":nth-child(15)")
+    abilities = row.select_one(":nth-child(16)")
+
+    set_unit_abilities(unit, magic, abilities)
+
+    print("jednotka: ", unit)
+
+
+def set_unit_abilities(unit:dict , magic: str, abilities:str) -> None:
+    magic = magic.findAll("img")
+    abilities = abilities.findAll("img")
+
+    for spell in magic:
+        pass
+
+    for ability in abilities:
+        pass
 
 
 def set_fraction(row: str) -> str:
@@ -49,6 +83,6 @@ def set_fraction(row: str) -> str:
 
 
 if __name__ == "__main__":
-    file_data = load_file()
+    file_data = load_file("jednotky_test.html")
 
     data = extract_data_from_html_table(file_data)
