a38cd5686be44be78cbb6bf96d58a7211984531e
Jakub <jochlik@email.cz>
2022-05-26 21:13:01 +0200

    refactor unit comparatoru

--- unit_parser/unit_xml_comparator.py
+++ unit_parser/unit_xml_comparator.py
@@ -1,21 +1,21 @@
+import sys
 import xml.etree.ElementTree as ET
 
 
 def find_missing_units(file_path1, file_path2):
-    file1 = ET.parse(file_path1)
-    file2 = ET.parse(file_path2)
+    original_xml = ET.parse(file_path1)
+    new_xml = ET.parse(file_path2)
 
-    root1 = file1.getroot()
-    root2 = file2.getroot()
+    original_xml = original_xml.getroot()
+    new_xml = new_xml.getroot()
 
     original_units = []
-
     added = []
 
-    for child in root1:
+    for child in original_xml:
         original_units.append(child[1].text.lower())
 
-    for child in root2:
+    for child in new_xml:
         if child[1].text.lower() not in original_units:
             added.append(child[1].text.lower())
         else:
@@ -26,7 +26,13 @@ def find_missing_units(file_path1, file_path2):
 
 
 if __name__ == "__main__":
-    xml_file1 = "../jednotky.xml"
-    xml_file2 = "generovane_jednotky.xml"
+    original_file = "../jednotky.xml"
+    new_file = "generovane_jednotky.xml"
 
-    find_missing_units(xml_file1, xml_file2)
+    if len(sys.argv) >= 2:
+        original_file = sys.argv[1]
+    if len(sys.argv) >= 3:
+        new_file = sys.argv[2]
+
+
+    find_missing_units(original_file, new_file)
