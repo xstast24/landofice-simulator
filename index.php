c94e8558ed7206d53f646ae25e84387fbd8312c4
Filip <filip.stastny@avast.com>
2022-05-29 01:16:33 +0200

    Posvatny chram vracen do pleneni

--- index.php
+++ index.php
@@ -137,11 +137,11 @@ table#form id {padding: 4px; color: white}
         <input value='Trpaslíci 2 - Strážní věž' class='pleneni zelene' type=button onclick='pole("684 x Železný kněz\n15,032 x Trpaslík\n1,122 x Železnej golem\n29,662 x Železný pavouček\n1,285 x Železný škorpion\n418 x Železný býk")'><br>
         <input value='Trpaslíci 3 - Citedela' class='pleneni zelene' type=button onclick='pole("1,166 x Železný kněz\n24,366 x Trpaslík\n1,513 x Kamenný chrlič\n562 x Poloautomatická socha\n145 x Železný orel - MK1\n152 x Obrněnej vrhač oštěpů\n132 x Obrněnej válečnej vůz")'><br>
         <input value='Trpaslíci 4 - Železný chrám' class='pleneni zelene' type=button onclick='pole("2,003 x Železný kněz\n45,297 x Trpaslík\n3,992 x Železný škorpion\n1,305 x Železný býk\n2,779 x Kamenný chrlič\n921 x Poloautomatická socha\n259 x Železný orel - MK1\n5 x Colosus z temné ocele")'><br>
+        <!--Posvatny chram je specialni event, ktery se odemce pouze pro Dreadda s Nekromancery-->
+        <input style='color:gold' value='Posvátný chrám' type=button onclick='pole("10000 x Svatý válečník\n1500 x Elitní střelec\n1000 x Mnich\n500 x Křižák\n250 x Paladin\n1 x Světlonoš\n25 x Balista\n30 x Těžká balista\n1000 x Renegád")'><br>
         <input value='Prokletý vojevůdce' class='pleneni cervene' type=button onclick='pole("1 x Dehinator (Ohnivá zbroj)\n12 x Těžký katapult\n12 x Těžká balista\n40 x Zeď Temné pevnosti\n300 x Prokletý Mág\n750 x Prokletý válečník(Prapor stínů)\n6 x Dehinatorova ochranka")'><br>
         <input value='Prokletá citadela' class='pleneni cervene' type=button onclick='pole("1 x Královna Medůz(Maska Královny medůz)\n1000 x Temný mág(Kouzelnická róba)\n200 x Vyšší Temný Mág(Prokletá kouzelnická róba)\n32 x Zeď Prokleté citadely\n200 x Prokletý Mág\n1000 x Prokletý válečník(Prapor stínů)\n3000 x Æthrův ledový válečník")'><br>
         <input value='Mucusova pevnost' class='pleneni cervene' type=button onclick='pole("1 x Mucus, král Toxických elementálů(Plášť Mucuse, krále toxických elementálů)\n1500 x Zeď pokrytá slizem\n1000 x Toxický Elementál Obránce\n1000 x Toxický Elementál Útočník\n10 x Toxická Elementární Bublina")'><br>
-        <!--Old: <input style='color:gold' value='Posvátný chrám' type=button onclick='pole("10000 x Svatý válečník\n1500 x Elitní střelec\n1000 x Mnich\n500 x Křižák\n250 x Paladin\n1 x Světlonoš\n25 x Balista\n30 x Těžká balista\n1000 x Renegád")'><br>-->
-
     </td>
 
 
