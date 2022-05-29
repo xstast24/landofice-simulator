f549e7507b719c1d30771bc07659f51c19400776
Filip Šťastný <filip.stastny@avast.com>
2022-05-30 00:34:07 +0200

    release package script

---
+++ create_release_package.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+PACKAGE_NAME="simulator-release"
+if [ -n "$1" ]; then  # if any arg given, show help
+  echo "Creates a release package '$PACKAGE_NAME' (dir & zip) that contains everything needed to run the simulator on the server."
+  echo "Zip package is also created for easy upload to LoI forum etc. Package is created only locally, not uploaded anywhere."
+  echo "Example usage (no params needed): ${FUNCNAME[0]}" && return
+fi
+
+RELEASE_FILES=(
+  images
+  ajax.js
+  ajax-loader.gif
+  favicon.ico
+  index.php
+  jednotky.php
+  jednotky.xml
+  pleneni.php
+  seznam.php
+  simulator_engine.php
+  sorttable.js
+  wz_tooltip.js
+)
+
+SCRIPT_DIR=$(dirname "${0}")
+SIMULATOR_ROOT="$SCRIPT_DIR"
+RELEASE_DIR="$SIMULATOR_ROOT"/"$PACKAGE_NAME"
+
+echo "Creating release dir"
+rm -rf "$RELEASE_DIR"
+mkdir -p "$RELEASE_DIR"
+# copy contents
+for path in "${RELEASE_FILES[@]}"; do
+  cp -a "$SIMULATOR_ROOT"/"$path" "$RELEASE_DIR"  # -a to recursively copy (also dirs) and preserve file attributes
+done
+
+echo "Creating release zip"
+zip -q -r "$SIMULATOR_ROOT"/"$PACKAGE_NAME".zip "$RELEASE_DIR"  # -q to not show all processed files
