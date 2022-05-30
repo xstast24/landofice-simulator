5b187b3594c963d7484266382428150526a51076
Filip Šťastný <filip.stastny@avast.com>
2022-05-30 15:45:59 +0200

    use html 5

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $obrance="1 x Dehinator (Ohnivá zbroj)
 6 x Dehinatorova ochranka";
 ?>
 
-<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">
+<!DOCTYPE html>
 <html>
 <head>
 <META http-equiv='Content-Type' content='text/html; charset=UTF-8'>
