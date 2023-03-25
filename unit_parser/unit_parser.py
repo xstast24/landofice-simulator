84badc162d8b64b0f26f6fe4a67ca1bfe6fc3bcb
Jakub <jochlik@email.cz>
2023-03-25 11:47:32 +0100

    update comment according to refactor

--- unit_parser/unit_parser.py
+++ unit_parser/unit_parser.py
@@ -43,12 +43,11 @@ def extract_data_from_html_table(raw_data: str) -> list:
         if row_length == 0:
             continue
 
-        # this if is needed to reset fraction setting after when any unit section from html doc has finished parsing
-        if row_length == 14:
+        if row_length == 14:    # setting fraction
             field_names = map(BeautifulSoup.get_text, row.findAll("th"))
             if "Jmeno" in field_names:
                 fraction = set_fraction(last_row)
-        elif row_length == 16:
+        elif row_length == 16:  # parsing unit
             unit_list.append(parse_html_row(row, fraction))
 
         last_row = row
